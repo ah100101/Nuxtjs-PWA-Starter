c5dc6c04e2bae91894a545a641ff6a68e5d4d070
ah100101 <ah100101@gmail.com>
2018-07-04 14:32:24 -0500

    search friends styling

--- pages/friends.vue
+++ pages/friends.vue
@@ -1,23 +1,54 @@
 <template lang='pug'>
-div friends
-</template>
+section.container.friends
+  .search
+    .field
+      p(class="control has-icons-left has-icons-right")
+        input(class="input" type="text" placeholder="Search current friends")
+        span(class="icon is-small is-left")
+          i.material-icons search
+  .columns.is-centered
+    .column
+</template>  
 
 <script>
+import authenticatedMixin from '~/mixins/authenticated.js'
+
 export default {
   asyncData (context) {
-    // called every time before loading the component
-    return { name: 'World' }
+    return 
+    { 
+      
+    }
   },
-  fetch () {
-    // The `fetch` method is used to fill the store before rendering the page
-  },
-  head () {
-    // Set Meta Tags for this Page
-  },
-  // and more functionality to discover
+  mixins: [
+    authenticatedMixin
+  ],
+  components: {
+    
+  }
 }
 </script>
 
 <style lang='scss'>
+@import '~/assets/scss/variables.scss';
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  font-family: $family-serif;
+}
+
+.search {
+  margin-top: 3.25rem;
+  width: 100%;
+
+  input {
+    border-radius: 0px;
+    border-left-width: 0px;
+    border-right-width: 0px;
+  }
+}
 
 </style>
