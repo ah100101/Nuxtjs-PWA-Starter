0232be11385a5452d94d871431e442e82dc2e51b
ah100101 <ah100101@gmail.com>
2018-07-07 15:10:33 -0500

    added post page and scaffolding

---
+++ pages/post.vue
@@ -0,0 +1,46 @@
+<template lang='pug'>
+section.post
+    | Post a product
+</template>  
+
+<script>
+import authenticatedMixin from '~/mixins/authenticated.js'
+
+export default {
+  asyncData (context) {
+    return 
+    { 
+      
+    }
+  },
+  mixins: [
+    authenticatedMixin
+  ],
+  components: {
+    
+  }
+}
+</script>
+
+<style lang='scss'>
+@import '~/assets/scss/variables.scss';
+
+.post {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: $family-serif;
+
+  .columns {
+    margin: 0;
+    width: 100%;
+  }
+
+  .column {
+    padding: 0;
+  }
+}
+
+</style>
